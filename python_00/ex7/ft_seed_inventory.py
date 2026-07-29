5e1615cc8e69050b50f5abbedb5801476797767a
gabriiel-lima <lima.gabriel@outlook.pt>
2026-07-29 10:55:47 +0100

    00 is done

--- python_00/ex7/ft_seed_inventory.py
+++ python_00/ex7/ft_seed_inventory.py
@@ -1,6 +1,11 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
-    if unit != "packets" and unit != " area" and unit != "grams":
+    if unit != "packets" and unit != "area" and unit != "grams":
         print("Unknown unit type")
-
-    print(f"{seed_type} seeds: {'cover' if unit == 'area' else ''} {quantity} {'square meters' if unit == 'area' else ''}{'packets available' if unit ==  'packets' else ''} {'grams total' if unit == 'grams' else ''}")
-    
+    else:
+        print(
+            f"{seed_type} seeds: {'cover' if unit == 'area' else ''} "
+            f"{quantity} "
+            f"{'square meters' if unit == 'area' else ''}"
+            f"{'packets available' if unit == 'packets' else ''}"
+            f"{'grams total' if unit == 'grams' else ''}"
+        )
